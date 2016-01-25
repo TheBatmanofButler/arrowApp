db9a58347e60ac39d03e55931888eaba9ee791ee
Ganesh Ravichandran <gr2483@columbia.edu>
2016-01-25 18:38:56 -0500

    fb login set up

---
+++ arrow/arrow.js
@@ -0,0 +1,148 @@
+Friends = new Mongo.Collection("friends");
+
+var isFBinit = false;
+
+currentUserData = {
+  name: null,
+  userID: null,
+  userAccessToken: null,
+  loginStatus: false,
+  proPicURL: null
+}
+
+var currentUserDep = new Tracker.Dependency();
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+
+}
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+
+  window.fbAsyncInit = function() {
+    console.log("fb initializing...");
+    FB.init({
+      appId : '510841125734589',
+      status : true,
+      xfbml : true
+    });
+    isFBinit = true;
+    console.log("fb initialized");
+
+    // triggered on fbLogin and fbLogout
+    FB.Event.subscribe('auth.authResponseChange', function(response) {
+      if (response.status === 'connected') {
+        currentUserData.userID = response.authResponse.userID;
+        currentUserData.userAccessToken = response.authResponse.accessToken;
+
+        FB.api('/', 'POST', {
+            batch: [
+              { method: 'GET', relative_url: 'me/friends'},
+              { method: "GET", relative_url: currentUserData.userID + '/picture'},
+              { method: "GET", relative_url: currentUserData.userID},
+            ]
+          },
+            function (response) {
+              if (response && !response.error) {
+                var friendsData = JSON.parse(response[0].body).data;                  
+                for (var key in friendsData) {
+                  var friend = friendsData[key];
+                  Friends.insert({
+                    name: friend.name,
+                    id: friend.id,
+                  });
+                }
+
+                currentUserData.proPicURL = response[1].headers[3].value;
+                currentUserData.name = JSON.parse(response[2].body).name;
+
+                currentUserDep.changed();
+              }
+            }
+        );
+
+        currentUserData.loginStatus = true;
+        currentUserDep.changed();
+      }
+    });
+  };
+
+  Template.login.helpers({
+
+    currentUserName: function() {
+      currentUserDep.depend();
+      return currentUserData.name;
+    },
+
+    // returns src for user profile picture to <img>
+    currentUserProPic: function() {
+      currentUserDep.depend();
+      return currentUserData.proPicURL;
+    }
+  });
+
+  Template.login.events({
+    'click .login-button': function (e) {
+      e.preventDefault();
+      Meteor.call('fbLogin');
+    },
+
+    'click .logout-button': function (e) {
+      e.preventDefault();
+      Meteor.call('fbLogout');
+    }
+  });
+
+  Template.registerHelper('isUserLoggedIn', function() {
+    currentUserDep.depend();
+    return currentUserData.loginStatus;
+  });
+
+  Template.body.helpers({
+    friends: function () {
+      return Friends.find({});
+    }
+  });
+
+  Template.body.events({
+
+  });
+
+  Template.friendList.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    },
+  });
+
+  Template.friendList.events({
+
+  });
+
+  Accounts.ui.config({
+  });
+}
+
+Meteor.methods({
+  // fbLogin: function() {
+  //   console.log('test');
+  // }
+
+  fbLogin: function() {
+    if (isFBinit){
+      FB.login();
+    }
+  },
+
+  fbLogout: function() {
+    if (isFBinit){
+      console.log('logged out');
+
+      FB.logout();
+      currentUserData.loginStatus = false;
+      currentUserDep.changed();
+    }
+  }
+
+});
